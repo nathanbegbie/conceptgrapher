c0888803b814f8e4ee9ae7c68eafb044e1126b47
nathanbegbie <nathanbegbie@gmail.com>
2015-08-15 21:26:52 +0200

    Created Translator file to parse data from .map file into Python objects and then into JSON format for D3.js processing

--- Translator.py
+++ Translator.py
@@ -2,7 +2,8 @@ import regex as re
 from Graph import Graph
 from Group import Group
 from Nodes import FactNode, ConceptNode, MisconNode, ScaseNode
-# from Edge import Edge
+import json
+import random
 
 
 class Translator:
@@ -13,7 +14,10 @@ class Translator:
         graph = Graph()
 
         # get the file
-        f = open('FinancialMaths.map', 'r')
+        try:
+            f = open('FinancialMaths.map', 'r')
+        except IOError:
+            print "file read error"
 
         # get the first set of the groups
         map_file = f.read()
@@ -47,10 +51,53 @@ class Translator:
             elif(type == "scase"):
                 graph.add_node(ScaseNode(ID=id, content=label))
             else:
-                print "Unkown data type"
+                print "Unknown data type"
 
         print graph.numNodes
 
         # add the edges
+        edge_dict = (re.findall(
+            r'[A-z]{4}\d{3,4}\s?->\s?[A-z]{4}\d{3,4}',
+            map_file, overlapped=True))
+        # edge cases
+        edge_dict += (re.findall(
+            r'[A-z]{3,4}\d{3,4}\s?/\*.*\*/\s?->\s?[A-z]{4}\d{3,4}',
+            map_file))
 
-        
+        for edge in edge_dict:
+            codes = re.findall(r'[A-z]{3,4}\d{3,4}', edge)
+            graph.add_edge(codes[0], codes[1])
+
+        print len(graph.nodeDict)
+
+        # create the array of node dict
+        nodes = []
+        # iterate through the nodes
+        for key, value in graph.nodeDict.iteritems():
+            nodes.append({"name": value.ID,
+                          "group": str(random.randint(1, 5))})
+
+        print len(nodes)
+
+        links = []
+        for i in range(0, len(nodes)):
+            # see if the node has any edges
+            # get the id of the graph, check if it is in the dict of edges
+            source_id = nodes[i]["name"]
+
+            if source_id in graph.edgeDict:
+                # iterate through the list of targets
+                # for each target, get target's position in the nodes array
+                for target_id in graph.edgeDict[source_id].targets:
+                    position = 0
+                    for j in range(0, len(nodes)):
+                        # check if we have found the position of the node
+                        if target_id == nodes[j]["name"]:
+                            position = j
+                            break
+                    links.append({"source": i, "target": position})
+            else:
+                print "node " + source_id + " has no edges"
+        data = {"nodes": nodes, "links": links}
+        with open('data.json', 'w') as outfile:
+            json.dump(data, outfile)
