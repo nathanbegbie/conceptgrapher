95b13cb313472f5d57c7de38a7c1f24b8ecd620a
Ethan Marrs <ethanmarrs@gmail.com>
2015-09-18 21:08:51 +0200

    Minor try/catch change

--- js/main-compiled.js
+++ js/main-compiled.js
@@ -49,185 +49,189 @@ var Visualizer = (function () {
       var link = visuals.selectAll(".link");
       var node = visuals.selectAll(".node");
 
-      // Start asynchronous callback for JSON data
-      $.getJSON('data.json').done(function (graph) {
+      try {
+        // Start asynchronous callback for JSON data
+        $.getJSON('data.json').done(function (graph) {
 
-        // Setup nodes and edges correctly for D3
-        var nodes = {};
-        var links = {};
+          // Setup nodes and edges correctly for D3
+          var nodes = {};
+          var links = {};
 
-        // Filter nodes based on group constraint
-        if (constraint !== null && constraint.length > 0) {
-          nodes = graph.nodes.filter(function (e) {
-            var _iteratorNormalCompletion = true;
-            var _didIteratorError = false;
-            var _iteratorError = undefined;
+          // Filter nodes based on group constraint
+          if (constraint !== null && constraint.length > 0) {
+            nodes = graph.nodes.filter(function (e) {
+              var _iteratorNormalCompletion = true;
+              var _didIteratorError = false;
+              var _iteratorError = undefined;
+
+              try {
+
+                for (var _iterator = e["group"][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+                  var i = _step.value;
+
+                  if (constraint.indexOf(i) >= 0) {
+                    return true;
+                  }
+                }
+              } catch (err) {
+                _didIteratorError = true;
+                _iteratorError = err;
+              } finally {
+                try {
+                  if (!_iteratorNormalCompletion && _iterator["return"]) {
+                    _iterator["return"]();
+                  }
+                } finally {
+                  if (_didIteratorError) {
+                    throw _iteratorError;
+                  }
+                }
+              }
+
+              return false;
+            });
+
+            // Filter links based on constrained nodes
+            links = graph.links.filter(function (e) {
+              if (search(e["source"], nodes) >= 0 && search(e["target"], nodes) >= 0) {
+                return true;
+              }
+            });
+
+            // Update links to use numerical IDs for D3
+            var _iteratorNormalCompletion2 = true;
+            var _didIteratorError2 = false;
+            var _iteratorError2 = undefined;
 
             try {
+              for (var _iterator2 = links[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
+                var i = _step2.value;
 
-              for (var _iterator = e["group"][Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-                var i = _step.value;
-
-                if (constraint.indexOf(i) >= 0) {
-                  return true;
-                }
+                i["source"] = search(i["source"], nodes);
+                i["target"] = search(i["target"], nodes);
               }
+
+              // Adding links and nodes to visualization
             } catch (err) {
-              _didIteratorError = true;
-              _iteratorError = err;
+              _didIteratorError2 = true;
+              _iteratorError2 = err;
             } finally {
               try {
-                if (!_iteratorNormalCompletion && _iterator["return"]) {
-                  _iterator["return"]();
+                if (!_iteratorNormalCompletion2 && _iterator2["return"]) {
+                  _iterator2["return"]();
                 }
               } finally {
-                if (_didIteratorError) {
-                  throw _iteratorError;
+                if (_didIteratorError2) {
+                  throw _iteratorError2;
                 }
               }
             }
 
-            return false;
-          });
+            force.nodes(nodes).links(links).start();
 
-          // Filter links based on constrained nodes
-          links = graph.links.filter(function (e) {
-            if (search(e["source"], nodes) >= 0 && search(e["target"], nodes) >= 0) {
-              return true;
-            }
-          });
+            // Adding links to SVG
+            link = link.data(links).enter().append("line").attr("class", function (d) {
+              return "link " + d["typeof"];
+            }).attr("marker-end", function (d) {
+              if (d["typeof"] === "directed") {
+                return "url(#arrowhead)";
+              } else {
+                return " ";
+              }
+            });
 
-          // Update links to use numerical IDs for D3
-          var _iteratorNormalCompletion2 = true;
-          var _didIteratorError2 = false;
-          var _iteratorError2 = undefined;
+            // Size of symbols
+            var scaleOfBigSymbols = 6;
+            var scaleOfSmallSymbols = 4;
+
+            // Adding nodes to SVG
+            node = node.data(nodes).enter().append("g").attr("class", function (f) {
+              return f.group.join(" ") + " " + f["typeof"] + " node";
+            }).attr("content", function (f) {
+              return f.content;
+            }).call(drag);
+
+            // Concept
+            d3.selectAll(".ConceptNode").append("path").attr("d", d3.svg.symbol().type("circle")).attr("transform", "scale(" + scaleOfBigSymbols + ")").on("dblclick", doubleClick).on("mouseover", mouseover).on("mouseout", mouseout);
+
+            // Fact
+            d3.selectAll(".FactNode").append("path").attr("d", d3.svg.symbol().type("square")).attr("transform", "scale(" + scaleOfBigSymbols + ")").on("dblclick", doubleClick).on("mouseover", mouseover).on("mouseout", mouseout);
+
+            // Scase
+            d3.selectAll(".ScaseNode").append("path").attr("d", d3.svg.symbol().type("cross")).attr("transform", "scale(" + scaleOfSmallSymbols + ")").on("dblclick", doubleClick).on("mouseover", mouseover).on("mouseout", mouseout);
+
+            // Misconcept
+            d3.selectAll(".MisconNode").append("path").attr("d", d3.svg.symbol().type("diamond")).attr("transform", "scale(" + scaleOfSmallSymbols + ")").on("dblclick", doubleClick).on("mouseover", mouseover).on("mouseout", mouseout);
+
+            // Text
+            node.append("svg:text").attr("class", "nodetext").attr("dx", 0).attr("dy", ".35em").attr("text-anchor", "middle").attr("fill", "black").text(function (d) {
+              return d.name;
+            });
+          } else {
+            $("#svg-wrapper").append("<div id='filter-text'><h5>To get started, use the filters on the right to select data.</h5></div>");
+          }
+
+          // Create filtering list
+          var uniqueGroup = [];
+
+          var _iteratorNormalCompletion3 = true;
+          var _didIteratorError3 = false;
+          var _iteratorError3 = undefined;
 
           try {
-            for (var _iterator2 = links[Symbol.iterator](), _step2; !(_iteratorNormalCompletion2 = (_step2 = _iterator2.next()).done); _iteratorNormalCompletion2 = true) {
-              var i = _step2.value;
+            for (var _iterator3 = graph.nodes[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
+              var i = _step3.value;
 
-              i["source"] = search(i["source"], nodes);
-              i["target"] = search(i["target"], nodes);
+              var group = i.group;
+              var _iteratorNormalCompletion4 = true;
+              var _didIteratorError4 = false;
+              var _iteratorError4 = undefined;
+
+              try {
+                for (var _iterator4 = group[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
+                  var j = _step4.value;
+
+                  if (uniqueGroup.indexOf(j) === -1) {
+                    uniqueGroup.push(j);
+                  }
+                }
+              } catch (err) {
+                _didIteratorError4 = true;
+                _iteratorError4 = err;
+              } finally {
+                try {
+                  if (!_iteratorNormalCompletion4 && _iterator4["return"]) {
+                    _iterator4["return"]();
+                  }
+                } finally {
+                  if (_didIteratorError4) {
+                    throw _iteratorError4;
+                  }
+                }
+              }
             }
-
-            // Adding links and nodes to visualization
           } catch (err) {
-            _didIteratorError2 = true;
-            _iteratorError2 = err;
+            _didIteratorError3 = true;
+            _iteratorError3 = err;
           } finally {
             try {
-              if (!_iteratorNormalCompletion2 && _iterator2["return"]) {
-                _iterator2["return"]();
+              if (!_iteratorNormalCompletion3 && _iterator3["return"]) {
+                _iterator3["return"]();
               }
             } finally {
-              if (_didIteratorError2) {
-                throw _iteratorError2;
+              if (_didIteratorError3) {
+                throw _iteratorError3;
               }
             }
           }
 
-          force.nodes(nodes).links(links).start();
-
-          // Adding links to SVG
-          link = link.data(links).enter().append("line").attr("class", function (d) {
-            return "link " + d["typeof"];
-          }).attr("marker-end", function (d) {
-            if (d["typeof"] === "directed") {
-              return "url(#arrowhead)";
-            } else {
-              return " ";
-            }
-          });
-
-          // Size of symbols
-          var scaleOfBigSymbols = 6;
-          var scaleOfSmallSymbols = 4;
-
-          // Adding nodes to SVG
-          node = node.data(nodes).enter().append("g").attr("class", function (f) {
-            return f.group.join(" ") + " " + f["typeof"] + " node";
-          }).attr("content", function (f) {
-            return f.content;
-          }).call(drag);
-
-          // Concept
-          d3.selectAll(".ConceptNode").append("path").attr("d", d3.svg.symbol().type("circle")).attr("transform", "scale(" + scaleOfBigSymbols + ")").on("dblclick", doubleClick).on("mouseover", mouseover).on("mouseout", mouseout);
-
-          // Fact
-          d3.selectAll(".FactNode").append("path").attr("d", d3.svg.symbol().type("square")).attr("transform", "scale(" + scaleOfBigSymbols + ")").on("dblclick", doubleClick).on("mouseover", mouseover).on("mouseout", mouseout);
-
-          // Scase
-          d3.selectAll(".ScaseNode").append("path").attr("d", d3.svg.symbol().type("cross")).attr("transform", "scale(" + scaleOfSmallSymbols + ")").on("dblclick", doubleClick).on("mouseover", mouseover).on("mouseout", mouseout);
-
-          // Misconcept
-          d3.selectAll(".MisconNode").append("path").attr("d", d3.svg.symbol().type("diamond")).attr("transform", "scale(" + scaleOfSmallSymbols + ")").on("dblclick", doubleClick).on("mouseover", mouseover).on("mouseout", mouseout);
-
-          // Text
-          node.append("svg:text").attr("class", "nodetext").attr("dx", 0).attr("dy", ".35em").attr("text-anchor", "middle").attr("fill", "black").text(function (d) {
-            return d.name;
-          });
-        } else {
-          $("#svg-wrapper").append("<div id='filter-text'><h5>To get started, use the filters on the right to select data.</h5></div>");
-        }
-
-        // Create filtering list
-        var uniqueGroup = [];
-
-        var _iteratorNormalCompletion3 = true;
-        var _didIteratorError3 = false;
-        var _iteratorError3 = undefined;
-
-        try {
-          for (var _iterator3 = graph.nodes[Symbol.iterator](), _step3; !(_iteratorNormalCompletion3 = (_step3 = _iterator3.next()).done); _iteratorNormalCompletion3 = true) {
-            var i = _step3.value;
-
-            var group = i.group;
-            var _iteratorNormalCompletion4 = true;
-            var _didIteratorError4 = false;
-            var _iteratorError4 = undefined;
-
-            try {
-              for (var _iterator4 = group[Symbol.iterator](), _step4; !(_iteratorNormalCompletion4 = (_step4 = _iterator4.next()).done); _iteratorNormalCompletion4 = true) {
-                var j = _step4.value;
-
-                if (uniqueGroup.indexOf(j) === -1) {
-                  uniqueGroup.push(j);
-                }
-              }
-            } catch (err) {
-              _didIteratorError4 = true;
-              _iteratorError4 = err;
-            } finally {
-              try {
-                if (!_iteratorNormalCompletion4 && _iterator4["return"]) {
-                  _iterator4["return"]();
-                }
-              } finally {
-                if (_didIteratorError4) {
-                  throw _iteratorError4;
-                }
-              }
-            }
+          if (rebuild === true) {
+            buildList(uniqueGroup);
           }
-        } catch (err) {
-          _didIteratorError3 = true;
-          _iteratorError3 = err;
-        } finally {
-          try {
-            if (!_iteratorNormalCompletion3 && _iterator3["return"]) {
-              _iterator3["return"]();
-            }
-          } finally {
-            if (_didIteratorError3) {
-              throw _iteratorError3;
-            }
-          }
-        }
-
-        if (rebuild === true) {
-          buildList(uniqueGroup);
-        }
-      });
+        });
+      } catch (e) {
+        console.log(e.name);
+      }
 
       /* ------ D3 Helper Methods ------*/
 
